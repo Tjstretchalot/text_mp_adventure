709a25d9a08d5c8137bb0e26dc6a047af7e4d149
Timothy Moore <mtimothy984@gmail.com>
2018-08-27 11:29:48 -0700

    Mention light level in /look
    Now that light level does something it's worth telling the user about 
    it!

--- classes/listeners/local/look/current_location.lua
+++ classes/listeners/local/look/current_location.lua
@@ -8,8 +8,18 @@ local prototype = require('prototypes/prototype')
 require('prototypes/listener')
 -- endregion
 
-
 local LocalLookEventCurrentLocation = {}
+
+local function pretty_light_level(game_ctx, light_level)
+  if light_level == 'inside_dark' then return 'Darkness' end
+  if light_level == 'inside_electricity' then return 'Brightly lit interior' end
+  if light_level == 'outside' then
+    if game_ctx.day.is_day then return 'Outside (day)' end
+    return 'Outside (night)'
+  end
+
+  error('unknown light level: ' .. light_level, 2)
+end
 
 function LocalLookEventCurrentLocation:get_events() return { LocalLookEvent = true } end
 function LocalLookEventCurrentLocation:is_prelistener() return true end
@@ -25,14 +35,16 @@ function LocalLookEventCurrentLocation:process(game_ctx, local_ctx, networking, 
   if #advn.locations < 1 then return end
 
   if #advn.locations == 1 then
-    local loc = advn.locations[1]
-    event:append_line('You are at ' .. loc)
-    event:append_indented_line(game_ctx.locations[loc].description, 2)
+    local loc_nm = advn.locations[1]
+    local loc = game_ctx.locations[loc_nm]
+    event:append_line('You are at ' .. loc_nm .. ' | ' .. pretty_light_level(game_ctx, loc.lighting))
+    event:append_indented_line(loc.description, 2)
   else
     event:append_line('You are in multiple locations:')
-    for _,loc in ipairs(advn.locations) do
-      event:append_indented_line(loc, 2)
-      event:append_indented_line(game_ctx.locations[loc].description, 4)
+    for _,loc_nm in ipairs(advn.locations) do
+      local loc = game_ctx.locations[loc_nm]
+      event:append_indented_line(loc_nm .. ' | ' .. pretty_light_level(game_ctx, loc.lighting), 2)
+      event:append_indented_line(loc.description, 4)
     end
   end
 end
